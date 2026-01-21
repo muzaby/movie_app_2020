33296aab78cec8afb50ada855c35d7330b3d3948
daezaby <rlaeodud13@gmail.com>
2026-01-21 23:05:13 +0900

    05-1 클래스컴포넌트로 증감기능 만들기

--- src/App.js
+++ src/App.js
@@ -1,49 +1,25 @@
-import propTypes from "prop-types";
 
-function Food({ fav, image, rank }) {
-  return (
-    <div>
-      <h3>I love {fav}</h3>
-      <img src={image} alt={fav}/>
-      <p>Rank: {rank}</p>
-    </div>
-  );
-}
+import React from 'react';
 
-Food.prototype = {
-  fav: propTypes.string.isRequired,
-  image: propTypes.string.isRequired,
-  rank: propTypes.number.isRequired
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: "kimchi",
-    image: "https://assets.surlatable.com/m/15a89c2d9c6c1345/72_dpi_webp-REC-283110_Pizza-jpg",
-    rank: 5.0
-  },
-  {
-    id: 2,
-    name: "samgiopsal",
-    image: "https://i.namu.wiki/i/oFHlYDjoEh8f-cc3lNK9jAemRkbXxNGwUg7XiW5LGS6DF1P2x8GCeNQxbQhVIwtUS1u53YPw-uoyqpmLtrGNJA.webp",
-    rank: 4.5
-  },
-  {
-    id: 3,
-    name: "kimchi",
-    image: "https://i.namu.wiki/i/3u7KQ8jVXWJMJpioMNBo5bFb7NrMd1jUbJrGXy99vSgh57D2w0BHG3RlEnawqyRlfAuTihp37sPnlCKX3IlKNg.webp",
-    rank: 4.7
-  },
-];
-
-function App() {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-      {foodILike.map(dish => <Food key={dish.id} fav={dish.name} image={dish.image} rank={dish.rank} />)}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    console.log("add");
+  }
+  minus = () => {
+    console.log("minus");
+  }
+  render() {
+    return (
+      <div>
+        <h1>This number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
